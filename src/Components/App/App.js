8abef4009c2b62bcbe5951cc86280374686ebec7
joyce-shi <j224shi@uwaterloo.ca>
2020-05-07 18:14:29 -0400

    set up components

---
+++ src/Components/App/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Playlist from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlistName: 'playlistName',
+      playlistTracks: [{
+        name: 'name',
+        artist: 'artist',
+        album: 'album',
+        id: '1'
+      }],
+      searchResults: [{
+        name: 'name',
+        artist: 'artist',
+        album: 'album',
+        id: '1'
+      }, {
+        name: 'name2',
+        artist: 'artist2',
+        album: 'album2',
+        id: '2',
+      }] // tmp
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            <SearchBar/>
+            <div className="App-playlist">
+              <SearchResults searchResults={this.state.searchResults}/>
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            </div>
+          </div>
+      </div>
+    );
+}
+}
+
+export default App;
